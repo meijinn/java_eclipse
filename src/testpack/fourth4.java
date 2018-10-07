b086e01c47c968b08450ad77a6e6149e5ae425d8
takahashi-shoutarou <e1621@s.akashi.ac.jp>
2018-10-07 19:27:53 +0900

    opengl added.

--- src/testpack/fourth4.java
+++
@@ -1,5 +0,0 @@
-package testpack;
-
-public class fourth4 {
-
-}
---
+++ src/testpack/gltest.java
@@ -0,0 +1,54 @@
+package testpack;
+
+import com.jogamp.newt.event.WindowAdapter;
+import com.jogamp.newt.event.WindowEvent;
+import com.jogamp.newt.opengl.GLWindow;
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLCapabilities;
+import com.jogamp.opengl.GLEventListener;
+import com.jogamp.opengl.GLProfile;
+import com.jogamp.opengl.util.Animator;
+
+public class gltest implements GLEventListener
+{ //(1)
+
+	public static void main(String[] args) {
+		new gltest();
+	}
+
+	private Animator animator;
+
+	public gltest() {
+		GLCapabilities caps = new GLCapabilities(GLProfile.get(GLProfile.GL2));//(2)
+		GLWindow glWindow = GLWindow.create(caps); //(3)
+		glWindow.setTitle("First demo (Newt)"); //(4)
+		glWindow.setSize(300, 300); //(5)
+
+		glWindow.addWindowListener(new WindowAdapter() { //(6)
+			@Override
+			public void windowDestroyed(WindowEvent evt) {
+				System.exit(0);
+			}
+		});
+		glWindow.addGLEventListener(this); //(7)
+
+		Animator animator = new Animator(); //(8)
+		animator.add(glWindow);
+		animator.start();
+		glWindow.setVisible(true); //(10)
+	}
+
+	@Override
+	public void init(GLAutoDrawable drawable) {}
+
+	@Override
+	public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {}
+
+	@Override
+	public void display(GLAutoDrawable drawable) {}
+
+	@Override
+	public void dispose(GLAutoDrawable drawable) {
+		if(animator != null) animator.stop();
+	}
+}
